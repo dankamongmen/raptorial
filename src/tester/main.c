101cccac7be6415c0707ad32e87b0c7ee3cb33cb
nick black <nick.black@sprezzatech.com>
2013-01-25 19:03:35 -0500

    require an argument, and use it to specify parse

--- src/tester/main.c
+++ src/tester/main.c
@@ -1,13 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <raptorial.h>
 
-int main(void){
-	char *pcpath = NULL;
+static void
+usage(const char *name){
+	fprintf(stderr,"usage: %s packagesfile\n",name);
+}
+
+int main(int argc,char **argv){
 	struct pkgcache *pc;
 	int err;
 
-	if((pc = parse_packages_file(pcpath,&err)) == NULL){
+	if(argc != 2){
+		usage(argv[0]);
 		return EXIT_FAILURE;
 	}
+	if((pc = parse_packages_file(argv[1],&err)) == NULL){
+		fprintf(stderr,"Couldn't parse %s (%s?)\n",argv[1],strerror(err));
+		return EXIT_FAILURE;
+	}
+	printf("Successfully parsed %s\n",argv[1]);
 	return EXIT_SUCCESS;
 }
