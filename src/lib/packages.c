592485a16e552719f323f24ddfa841df2f6ba770
nick black <nick.black@sprezzatech.com>
2013-01-25 19:00:14 -0500

    check basic params in parse_packages_*()

--- src/lib/packages.c
+++ src/lib/packages.c
@@ -1,1 +1,24 @@
+#include <errno.h>
 #include <pthread.h>
+#include <raptorial.h>
+
+typedef struct pkgcache {
+} pkgcache;
+
+PUBLIC pkgcache *
+parse_packages_file(const char *path,int *err){
+	if(path == NULL){
+		*err = EINVAL;
+		return NULL;
+	}
+	return NULL;
+}
+
+PUBLIC pkgcache *
+parse_packages_mem(const void *mem,size_t len,int *err){
+	if(mem == NULL || len == 0){
+		*err = EINVAL;
+		return NULL;
+	}
+	return NULL;
+}
