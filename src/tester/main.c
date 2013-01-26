c90250a598321c24ba2aef54a876878eb96bab57
nick black <nick.black@sprezzatech.com>
2013-01-25 19:00:04 -0500

    go ahead and parse something in tester

--- src/tester/main.c
+++ src/tester/main.c
@@ -2,5 +2,12 @@
 #include <raptorial.h>
 
 int main(void){
+	char *pcpath = NULL;
+	struct pkgcache *pc;
+	int err;
+
+	if((pc = parse_packages_file(pcpath,&err)) == NULL){
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
