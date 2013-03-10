e225d3b44ef84cbaaae37e18584b972e41f6b4f3
nick black <nick.black@sprezzatech.com>
2013-03-10 08:03:29 -0400

    break file up into chunks amongst threads, fixing bug 671

--- src/lib/packages.c
+++ src/lib/packages.c
@@ -29,20 +29,13 @@ struct pkgparse {
 	const void *mem;
 	size_t len,csize;
 	pthread_mutex_t lock;
+
+	// These data are modified by threads, and must be protected by the
+	// lock. A thread takes the chunk at offset, incrementing offset by
+	// the chunksize. Parsed pkgobjs are placed in sharedpcache.
+	size_t offset;
 	pkgcache *sharedpcache;
 };
-
-// Threads handle chunks of the packages list. Since we don't know where 
-// package definitions are split in the list data, we'll usually toss some data
-// from the front (it was handled as part of another chunk), and grab some
-// extra data from the back. These will typically be small amounts, and the
-// data is read-only, so rather than track splits (requiring synchronization), 
-// we just always locally discover the bounds (put another way, the overlapping
-// areas at the front and back of chunks are lexed twice).
-typedef struct pkgchunk {
-	size_t offset;
-	struct pkgparse *pp;
-} pkgchunk;
 
 static void
 free_package(pkgobj *po){
@@ -73,132 +66,157 @@ create_package(const char *name,size_t namelen,const char *ver,size_t verlen){
 	return po;
 }
 
+static size_t
+get_new_offset(struct pkgparse *pp){
+	size_t o;
+
+	pthread_mutex_lock(&pp->lock);
+	o = pp->offset;
+	if(pp->len - pp->offset < pp->csize){ // Avoids possible overflow
+		pp->offset = pp->len;
+	}else{
+		pp->offset += pp->csize;
+	}
+	pthread_mutex_unlock(&pp->lock);
+	return o;
+}
+
+// Threads handle chunks of the packages list. Since we don't know where
+// package definitions are split in the list data, we'll usually toss some data
+// from the front (it was handled as part of another chunk), and grab some
+// extra data from the back. These will typically be small amounts, and the
+// data is read-only, so rather than track splits (requiring synchronization),
+// we just always locally discover the bounds (put another way, the overlapping
+// areas at the front and back of chunks are lexed twice).
+//
 // We're not treating the input as anything but ASCII text, though it's almost
 // certainly UTF8. Need to ensure that newlines and pattern tags are all
 // strictly ASCII or change how we handle things FIXME.
 static void *
 parse_chunk(void *vpp){
 	const char *start,*c,*end,*delim,*pname,*pver;
-	pkgchunk pc = {
-		.pp = vpp,
-		.offset = 0, // FIXME
-	};
+	struct pkgparse *pp = vpp;
+	size_t offset = 0; // FIXME
 	unsigned packages = 0;
 	pkgobj *head,*po,**enq;
 	size_t pnamelen,pverlen;
 	int state;
 
-	start = (const char *)pc.pp->mem + pc.offset;
-	if(pc.pp->csize + pc.offset > pc.pp->len){
-		end = start + (pc.pp->len - pc.offset);
-	}else{
-		end = start + pc.pp->csize;
-	}
-
-
-	// First, find the start of our chunk:
-	//  - If we are offset 0, we are at the start of our chunk
-	//  - Otherwise, if the previous two characters (those preceding our
-	//     chunk) are newlines, we are at the start of our chunk,
-	//  - Otherwise, if the first character is a newline, and the previous
-	//     character is a newline, we are at the start of our chunk
-	//  - Otherwise, our chunk starts at the first double newline
-	if(pc.offset){
-		// We can be in one of two states: we know the previous
-		// character to have been a newline, or we don't.
-		state = 0;
-		assert(pc.pp->csize > 2); // sanity check
-		for(c = start - 2 ; c < end ; ++c){
-			if(*c == '\n'){
-				if(state){
-					++c;
-					break;
-				}
-				state = 1;
-			}else{
-				state = 0;
-			}
-		}
-	}else{
-		c = start;
-	}
-
-
-	enq = &head;
 	head = NULL;
-	// We are at the beginning of our chunk, which might be 0 bytes. Any
-	// partial record with which our map started has been skipped
-	state = 2; // number of newlines we've seen, bounded by 2
-	// Upon reaching the (optional, only one allowed) delimiter on each
-	// line, delim will be updated to point one past that delimiter (which
-	// might be outside the chunk!), and to chew whitespace.
-	delim = NULL;
-	// We hand create_package our raw map bytes; it allocates the destbuf.
-	// These are thus reset on each package.
-	pname = NULL; pver = NULL;
-	pnamelen = 0; pverlen = 0;
-	while(c < end){
-		if(*c == '\n'){ // State machine is driven by newlines
-			if(++state == 2){
-				if(pname == NULL || pnamelen == 0){
-					goto err; // No package name
+	offset = get_new_offset(pp);
+	while(offset < pp->len){
+		start = (const char *)pp->mem + offset;
+		if(pp->csize + offset > pp->len){
+			end = start + (pp->len - offset);
+		}else{
+			end = start + pp->csize;
+		}
+
+		// First, find the start of our chunk:
+		//  - If we are offset 0, we are at the start of our chunk
+		//  - Otherwise, if the previous two characters (those preceding our
+		//     chunk) are newlines, we are at the start of our chunk,
+		//  - Otherwise, if the first character is a newline, and the previous
+		//     character is a newline, we are at the start of our chunk
+		//  - Otherwise, our chunk starts at the first double newline
+		if(offset){
+			// We can be in one of two states: we know the previous
+			// character to have been a newline, or we don't.
+			state = 0;
+			assert(pp->csize > 2); // sanity check
+			for(c = start - 2 ; c < end ; ++c){
+				if(*c == '\n'){
+					if(state){
+						++c;
+						break;
+					}
+					state = 1;
+				}else{
+					state = 0;
 				}
-				if(pver == NULL || pverlen == 0){
-					goto err; // No package version
-				}
-				if((po = create_package(pname,pnamelen,pver,pverlen)) == NULL){
-					goto err;
-				}
-				// Package ended!
-				++packages;
-				*enq = po;
-				enq = &po->next;
-				pname = NULL;
-				pver = NULL;
-			}else{ // We processed a line of the current package
-				if(delim){ // Was line delimited?
-					if((size_t)(c - start) >= strlen("Package:")){
-						if(strncmp(start,"Package:",strlen("Package:")) == 0){
-// Don't allow a package to be named twice. Defined another way, require
-// an empty line between every two instances of a Package: line.
-							if(pname){
-								goto err;
+			}
+		}else{
+			c = start;
+		}
+
+		enq = &head;
+		// We are at the beginning of our chunk, which might be 0 bytes. Any
+		// partial record with which our map started has been skipped
+		state = 2; // number of newlines we've seen, bounded by 2
+		// Upon reaching the (optional, only one allowed) delimiter on each
+		// line, delim will be updated to point one past that delimiter (which
+		// might be outside the chunk!), and to chew whitespace.
+		delim = NULL;
+		// We hand create_package our raw map bytes; it allocates the destbuf.
+		// These are thus reset on each package.
+		pname = NULL; pver = NULL;
+		pnamelen = 0; pverlen = 0;
+		while(c < end){
+			if(*c == '\n'){ // State machine is driven by newlines
+				if(++state == 2){
+					if(pname == NULL || pnamelen == 0){
+						goto err; // No package name
+					}
+					if(pver == NULL || pverlen == 0){
+						goto err; // No package version
+					}
+					if((po = create_package(pname,pnamelen,pver,pverlen)) == NULL){
+						goto err;
+					}
+					// Package ended!
+					++packages;
+					*enq = po;
+					enq = &po->next;
+					pname = NULL;
+					pver = NULL;
+				}else{ // We processed a line of the current package
+					if(delim){ // Was line delimited?
+						if((size_t)(c - start) >= strlen("Package:")){
+							if(strncmp(start,"Package:",strlen("Package:")) == 0){
+	// Don't allow a package to be named twice. Defined another way, require
+	// an empty line between every two instances of a Package: line.
+								if(pname){
+									goto err;
+								}
+								pnamelen = c - delim;
+								pname = delim;
+							}else if(strncmp(start,"Version:",strlen("Version:")) == 0){
+								if(pver){
+									goto err;
+								}
+								pverlen = c - delim;
+								pver = delim;
 							}
-							pnamelen = c - delim;
-							pname = delim;
-						}else if(strncmp(start,"Version:",strlen("Version:")) == 0){
-							if(pver){
-								goto err;
-							}
-							pverlen = c - delim;
-							pver = delim;
 						}
 					}
 				}
-			}
-		}else{ // not a newline
-			if(state){
-				delim = NULL;
-				start = c;
-				state = 0;
-			}
-			if(isspace(*c)){
-				if(c == delim){
-					++delim;
+			}else{ // not a newline
+				if(state){
+					delim = NULL;
+					start = c;
+					state = 0;
 				}
-			}else if(*c == ':'){
-				delim = c + 1;
+				if(isspace(*c)){
+					if(c == delim){
+						++delim;
+					}
+				}else if(*c == ':'){
+					delim = c + 1;
+				}
 			}
+			++c;
 		}
-		++c;
+		offset = get_new_offset(pp);
 	}
 	// FIXME if we're in the middle of a record, we read into the next map
 	// (hence why we skipped one if we started in media res)
-	pthread_mutex_lock(&pc.pp->lock); // Success!
-		pc.pp->sharedpcache->pcount += packages;
-		*enq = pc.pp->sharedpcache->pobjs;
-		pc.pp->sharedpcache->pobjs = head;
-	pthread_mutex_unlock(&pc.pp->lock);
+	if(head){
+		pthread_mutex_lock(&pp->lock); // Success!
+			pp->sharedpcache->pcount += packages;
+			*enq = pp->sharedpcache->pobjs;
+			pp->sharedpcache->pobjs = head;
+		pthread_mutex_unlock(&pp->lock);
+	}
 	return vpp;
 
 err:
@@ -215,8 +233,9 @@ parse_map(pkgcache *pc,const void *mem,size_t len,int *err){
 	struct pkgparse pp = {
 		.mem = mem,
 		.len = len,
-		.csize = len, // FIXME
+		.offset = 0,
 		.sharedpcache = pc,
+		.csize = 1024 * 1024,
 	};
 	blossom_ctl bctl = {
 		.flags = 0,
