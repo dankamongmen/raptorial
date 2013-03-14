6a550644b41d758f36f570287de64fa9c3fbf8bf
nick black <nick.black@sprezzatech.com>
2013-03-14 16:03:55 -0400

    differentiate between status files and release lists

--- src/lib/packages.c
+++ src/lib/packages.c
@@ -51,6 +51,8 @@ struct pkgparse {
 	unsigned count;
 	const void *mem;
 	size_t len,csize;
+	// Are we a status file, or a package list?
+	int statusfile;
 	pthread_mutex_t lock;
 
 	// These data are modified by threads, and must be protected by the
@@ -112,6 +114,7 @@ enum {
 	STATE_DELIM,
 	STATE_VERSION_DELIMITED,
 	STATE_PACKAGE_DELIMITED,
+	STATE_STATUS_DELIMITED,
 };
 
 // Threads handle chunks of the packages list. Since we don't know where
@@ -131,9 +134,9 @@ enum {
 // strictly ASCII or change how we handle things FIXME.
 static int
 parse_chunk(size_t offset,const char *start,const char *end,
-			const char *veryend,pkgobj ***enq){
-	const char *expect,*pname,*pver,*c,*delim;
-	size_t pnamelen,pverlen;
+		const char *veryend,pkgobj ***enq,int statusfile){
+	const char *expect,*pname,*pver,*pstatus,*c,*delim;
+	size_t pnamelen,pverlen,pstatuslen;
 	int rewardstate,state;
 	unsigned newp = 0;
 	pkgobj *po;
@@ -173,17 +176,24 @@ parse_chunk(size_t offset,const char *start,const char *end,
 	expect = NULL;
 	// We hand create_package our raw map bytes; it allocates the destbuf.
 	// These are thus reset on each package.
-	pname = NULL; pver = NULL;
-	pnamelen = 0; pverlen = 0;
+	pname = NULL; pver = NULL; pstatus = NULL;
 	state = STATE_RESET; // number of newlines we've seen, bounded by 2
 	while(c < end || (state != STATE_RESET && c < veryend)){
 		if(*c == '\n'){ // State machine is driven by newlines
-			if(state == STATE_NLINE){ // double newline
+			switch(state){
+			case STATE_NLINE:{ // double newline
 				if(pname == NULL || pnamelen == 0){
 					return -1; // No package name
 				}
 				if(pver == NULL || pverlen == 0){
 					return -1; // No package version
+				}
+				if(statusfile){
+					if(pstatus == NULL || pstatuslen == 0){
+						return -1; // No package status
+					}
+				}else if(pstatus){
+					return -1; // Status in package list
 				}
 				if((po = create_package(pname,pnamelen,pver,pverlen)) == NULL){
 					return -1;
@@ -194,23 +204,37 @@ parse_chunk(size_t offset,const char *start,const char *end,
 				*enq = &po->next;
 				pname = NULL;
 				pver = NULL;
+				pstatus = NULL;
 				state = STATE_RESET;
-			}else{ // We processed a line of the current package
-				if(state == STATE_PACKAGE_DELIMITED){
+				break;
+			} // Else we processed a line of the current package
+			case STATE_PACKAGE_DELIMITED:
 // Don't allow a package to be named twice. Defined another way, require an
 // empty line between every two instances of a Package: line.
-					if(pname){
-						return -1;
-					}
-					pnamelen = c - delim;
-					pname = delim;
-				}else if(state == STATE_VERSION_DELIMITED){
-					if(pver){
-						return -1;
-					}
-					pverlen = c - delim;
-					pver = delim;
+				if(pname){
+					return -1;
 				}
+				pnamelen = c - delim;
+				pname = delim;
+				state = STATE_NLINE;
+				break;
+			case STATE_VERSION_DELIMITED:
+				if(pver){
+					return -1;
+				}
+				pverlen = c - delim;
+				pver = delim;
+				state = STATE_NLINE;
+				break;
+			case STATE_STATUS_DELIMITED:
+				if(pstatus){
+					return -1;
+				}
+				pstatuslen = c - delim;
+				pstatus = delim;
+				break;
+			}
+			if(state != STATE_RESET){
 				state = STATE_NLINE;
 			}
 		}else switch(state){ // not a newline
@@ -226,6 +250,10 @@ parse_chunk(size_t offset,const char *start,const char *end,
 					state = STATE_EXPECT;
 					expect = "ackage:";
 					rewardstate = STATE_PACKAGE_DELIMITED;
+				}else if(*c == 'S'){
+					state = STATE_EXPECT;
+					expect = "tatus:";
+					rewardstate = STATE_STATUS_DELIMITED;
 				}else{
 					state = STATE_PDATA;
 				}
@@ -279,7 +307,7 @@ parse_chunks(void *vpp){
 		}else{
 			end = start + pp->csize;
 		}
-		newp = parse_chunk(offset,start,end,veryend,&enq);
+		newp = parse_chunk(offset,start,end,veryend,&enq,pp->statusfile);
 		if(newp < 0){
 			goto err;
 		}
@@ -308,6 +336,250 @@ struct dirparse {
 	pkgcache *sharedpcache;
 	pthread_mutex_t lock;
 };
+
+// len is the true length, less than or equal to the mapped length.
+static int
+parse_map(pkglist *pl,const void *mem,size_t len,int *err,int statusfile){
+	struct pkgparse pp = {
+		.statusfile = statusfile,
+		.mem = mem,
+		.len = len,
+		.offset = 0,
+		.sharedpcache = pl,
+		.csize = 1024 * 1024,
+	};
+	blossom_ctl bctl = {
+		.flags = 0,
+		.tids = 1,
+	};
+	blossom_state bs;
+	int r;
+
+	if( (r = pthread_mutex_init(&pp.lock,NULL)) ){
+		*err = r;
+		return -1;
+	}
+	if(blossom_per_pe(&bctl,&bs,NULL,parse_chunks,&pp)){
+		*err = errno;
+		pthread_mutex_destroy(&pp.lock);
+		return -1;
+	}
+	if(blossom_join_all(&bs)){
+		*err = errno;
+		blossom_free_state(&bs);
+		pthread_mutex_destroy(&pp.lock);
+		return -1;
+	}
+	if(blossom_validate_joinrets(&bs)){
+		blossom_free_state(&bs);
+		pthread_mutex_destroy(&pp.lock);
+		return -1;
+	}
+	blossom_free_state(&bs);
+	if((r = pthread_mutex_destroy(&pp.lock))){
+		*err = r;
+		return -1;
+	}
+	return 0;
+}
+
+static inline pkglist *
+create_pkglist(const void *mem,size_t len,int *err,int statusfile){
+	pkglist *pl;
+
+	if((pl = malloc(sizeof(*pl))) == NULL){
+		*err = errno;
+	}else{
+		memset(pl,0,sizeof(*pl));
+		if(parse_map(pl,mem,len,err,statusfile)){
+			free(pl);
+			return NULL;
+		}
+	}
+	return pl;
+}
+
+static inline pkgcache *
+create_pkgcache(pkglist *pl,int *err){
+	pkgcache *pc;
+
+	if((pc = malloc(sizeof(*pc))) == NULL){
+		*err = errno;
+	}else{
+		pc->lists = pl;
+	}
+	return pc;
+}
+
+static pkglist *
+parse_packages_file_internal(const char *path,int *err,int statusfile){
+	const void *map;
+	size_t mlen,len;
+	struct stat st;
+	pkglist *pl;
+	int fd,pg;
+
+	if(path == NULL){
+		*err = EINVAL;
+		return NULL;
+	}
+	// Probably ought get the largest page size; this will be the smallest
+	// on all platforms of which I'm aware. FIXME
+	if((pg = sysconf(_SC_PAGE_SIZE)) <= 0){
+		*err = errno;
+		return NULL;
+	}
+	if((fd = open(path,O_CLOEXEC)) < 0){
+		*err = errno;
+		return NULL;
+	}
+	if(fstat(fd,&st)){
+		*err = errno;
+		close(fd);
+		return NULL;
+	}
+	mlen = len = st.st_size;
+	if(mlen % pg != mlen / pg){
+		mlen = (mlen / pg) * pg + pg;
+	}
+	if((map = mmap(NULL,mlen,PROT_READ,MAP_SHARED|MAP_HUGETLB|MAP_POPULATE,fd,0)) == MAP_FAILED){
+		if(errno != EINVAL){
+			*err = errno;
+			close(fd);
+			return NULL;
+		}
+		// Try again without MAP_HUGETLB
+		if((map = mmap(NULL,mlen,PROT_READ,MAP_SHARED|MAP_POPULATE,fd,0)) == MAP_FAILED){
+			*err = errno;
+			close(fd);
+			return NULL;
+		}
+	}
+	if((pl = create_pkglist(map,len,err,statusfile)) == NULL){
+		close(fd);
+		return NULL;
+	}
+	close(fd);
+	return pl;
+}
+
+PUBLIC pkglist *
+parse_packages_file(const char *path,int *err){
+	return parse_packages_file_internal(path,err,0);
+}
+
+PUBLIC pkglist *
+parse_packages_mem(const void *mem,size_t len,int *err){
+	pkglist *pl;
+
+	if(mem == NULL || len == 0){
+		*err = EINVAL;
+		return NULL;
+	}
+	if((pl = create_pkglist(mem,len,err,0)) == NULL){
+		return NULL;
+	}
+	return pl;
+}
+
+PUBLIC void
+free_package_list(pkglist *pl){
+	if(pl){
+		free(pl->distribution);
+		free(pl->arch);
+		free(pl);
+	}
+}
+
+PUBLIC pkgcache *
+pkgcache_from_pkglist(pkglist *pl,int *err){
+	pkgcache *pc;
+
+	if(pl == NULL){
+		return NULL;
+	}
+	if((pc = create_pkgcache(pl,err)) == NULL){
+		free_package_list(pl);
+	}
+	return pc;
+}
+
+PUBLIC void
+free_package_cache(pkgcache *pc){
+	if(pc){
+		pkglist *pl;
+
+		while( (pl = pc->lists) ){
+			pc->lists = pl->next;
+			free_package_list(pl);
+		}
+		free(pc);
+	}
+}
+
+PUBLIC pkglist *
+parse_status_file(const char *path,int *err){
+	return parse_packages_file_internal(path,err,1);
+}
+
+PUBLIC pkglist *
+pkgcache_begin(pkgcache *pc){
+	return pc->lists;
+}
+
+PUBLIC pkglist *
+pkgcache_next(pkglist *pl){
+	return pl->next;
+}
+
+PUBLIC pkgobj *
+pkglist_begin(pkglist *pl){
+	return pl->pobjs;
+}
+
+PUBLIC pkgobj *
+pkglist_next(pkgobj *po){
+	return po->next;
+}
+
+PUBLIC const pkgobj *
+pkglist_cbegin(const pkglist *pl){
+	return pl->pobjs;
+}
+
+PUBLIC const pkgobj *
+pkglist_cnext(const pkgobj *po){
+	return po->next;
+}
+
+PUBLIC const char *
+pkgobj_name(const pkgobj *po){
+	return po->name;
+}
+
+PUBLIC const char *
+pkglist_dist(const pkglist *pl){
+	return pl->distribution;
+}
+
+PUBLIC const char *
+pkgobj_version(const pkgobj *po){
+	return po->version;
+}
+
+PUBLIC unsigned
+pkgcache_count(const pkgcache *pc){
+	unsigned tot = 0;
+
+	if(pc){
+		pkglist *pl;
+
+		for(pl = pc->lists ; pl ; pl = pl ->next){
+			tot += pl->pcount;
+		}
+	}
+	return tot;
+}
 
 static void *
 parse_dir(void *vdp){
@@ -339,7 +611,7 @@ parse_dir(void *vdp){
 			if(strcmp(dent.d_name + strlen(dent.d_name) - strlen(*suffix),*suffix) == 0){
 				int err;
 
-				if((pl = parse_packages_file(dent.d_name,&err)) == NULL){
+				if((pl = parse_packages_file_internal(dent.d_name,&err,0)) == NULL){
 					return NULL;
 				}
 				if((pl->distribution = strndup(dist,distdelim - dist)) == NULL){
@@ -400,131 +672,6 @@ parse_listdir(pkgcache *pc,DIR *dir,int *err){
 	return 0;
 }
 
-// len is the true length, less than or equal to the mapped length.
-static int
-parse_map(pkglist *pl,const void *mem,size_t len,int *err){
-	struct pkgparse pp = {
-		.mem = mem,
-		.len = len,
-		.offset = 0,
-		.sharedpcache = pl,
-		.csize = 1024 * 1024,
-	};
-	blossom_ctl bctl = {
-		.flags = 0,
-		.tids = 1,
-	};
-	blossom_state bs;
-	int r;
-
-	if( (r = pthread_mutex_init(&pp.lock,NULL)) ){
-		*err = r;
-		return -1;
-	}
-	if(blossom_per_pe(&bctl,&bs,NULL,parse_chunks,&pp)){
-		*err = errno;
-		pthread_mutex_destroy(&pp.lock);
-		return -1;
-	}
-	if(blossom_join_all(&bs)){
-		*err = errno;
-		blossom_free_state(&bs);
-		pthread_mutex_destroy(&pp.lock);
-		return -1;
-	}
-	if(blossom_validate_joinrets(&bs)){
-		blossom_free_state(&bs);
-		pthread_mutex_destroy(&pp.lock);
-		return -1;
-	}
-	blossom_free_state(&bs);
-	if((r = pthread_mutex_destroy(&pp.lock))){
-		*err = r;
-		return -1;
-	}
-	return 0;
-}
-
-static inline pkglist *
-create_pkglist(const void *mem,size_t len,int *err){
-	pkglist *pl;
-
-	if((pl = malloc(sizeof(*pl))) == NULL){
-		*err = errno;
-	}else{
-		memset(pl,0,sizeof(*pl));
-		if(parse_map(pl,mem,len,err)){
-			free(pl);
-			return NULL;
-		}
-	}
-	return pl;
-}
-
-static inline pkgcache *
-create_pkgcache(pkglist *pl,int *err){
-	pkgcache *pc;
-
-	if((pc = malloc(sizeof(*pc))) == NULL){
-		*err = errno;
-	}else{
-		pc->lists = pl;
-	}
-	return pc;
-}
-
-PUBLIC pkglist *
-parse_packages_file(const char *path,int *err){
-	const void *map;
-	size_t mlen,len;
-	struct stat st;
-	pkglist *pl;
-	int fd,pg;
-
-	if(path == NULL){
-		*err = EINVAL;
-		return NULL;
-	}
-	// Probably ought get the largest page size; this will be the smallest
-	// on all platforms of which I'm aware. FIXME
-	if((pg = sysconf(_SC_PAGE_SIZE)) <= 0){
-		*err = errno;
-		return NULL;
-	}
-	if((fd = open(path,O_CLOEXEC)) < 0){
-		*err = errno;
-		return NULL;
-	}
-	if(fstat(fd,&st)){
-		*err = errno;
-		close(fd);
-		return NULL;
-	}
-	mlen = len = st.st_size;
-	if(mlen % pg != mlen / pg){
-		mlen = (mlen / pg) * pg + pg;
-	}
-	if((map = mmap(NULL,mlen,PROT_READ,MAP_SHARED|MAP_HUGETLB|MAP_POPULATE,fd,0)) == MAP_FAILED){
-		if(errno != EINVAL){
-			*err = errno;
-			close(fd);
-			return NULL;
-		}
-		// Try again without MAP_HUGETLB
-		if((map = mmap(NULL,mlen,PROT_READ,MAP_SHARED|MAP_POPULATE,fd,0)) == MAP_FAILED){
-			*err = errno;
-			close(fd);
-			return NULL;
-		}
-	}
-	if((pl = create_pkglist(map,len,err)) == NULL){
-		close(fd);
-		return NULL;
-	}
-	close(fd);
-	return pl;
-}
-
 PUBLIC pkgcache *
 parse_packages_dir(const char *dir,int *err){
 	pkgcache *pc;
@@ -555,117 +702,4 @@ parse_packages_dir(const char *dir,int *err){
 		return NULL;
 	}
 	return pc;
-}
-
-PUBLIC pkglist *
-parse_packages_mem(const void *mem,size_t len,int *err){
-	pkglist *pl;
-
-	if(mem == NULL || len == 0){
-		*err = EINVAL;
-		return NULL;
-	}
-	if((pl = create_pkglist(mem,len,err)) == NULL){
-		return NULL;
-	}
-	return pl;
-}
-
-PUBLIC void
-free_package_list(pkglist *pl){
-	if(pl){
-		free(pl->distribution);
-		free(pl->arch);
-		free(pl);
-	}
-}
-
-PUBLIC pkgcache *
-pkgcache_from_pkglist(pkglist *pl,int *err){
-	pkgcache *pc;
-
-	if(pl == NULL){
-		return NULL;
-	}
-	if((pc = create_pkgcache(pl,err)) == NULL){
-		free_package_list(pl);
-	}
-	return pc;
-}
-
-PUBLIC void
-free_package_cache(pkgcache *pc){
-	if(pc){
-		pkglist *pl;
-
-		while( (pl = pc->lists) ){
-			pc->lists = pl->next;
-			free_package_list(pl);
-		}
-		free(pc);
-	}
-}
-
-PUBLIC pkglist *
-parse_status_file(const char *path,int *err){
-	return parse_packages_file(path,err);
-}
-
-PUBLIC pkglist *
-pkgcache_begin(pkgcache *pc){
-	return pc->lists;
-}
-
-PUBLIC pkglist *
-pkgcache_next(pkglist *pl){
-	return pl->next;
-}
-
-PUBLIC pkgobj *
-pkglist_begin(pkglist *pl){
-	return pl->pobjs;
-}
-
-PUBLIC pkgobj *
-pkglist_next(pkgobj *po){
-	return po->next;
-}
-
-PUBLIC const pkgobj *
-pkglist_cbegin(const pkglist *pl){
-	return pl->pobjs;
-}
-
-PUBLIC const pkgobj *
-pkglist_cnext(const pkgobj *po){
-	return po->next;
-}
-
-PUBLIC const char *
-pkgobj_name(const pkgobj *po){
-	return po->name;
-}
-
-PUBLIC const char *
-pkglist_dist(const pkglist *pl){
-	return pl->distribution;
-}
-
-PUBLIC const char *
-pkgobj_version(const pkgobj *po){
-	return po->version;
-}
-
-PUBLIC unsigned
-pkgcache_count(const pkgcache *pc){
-	unsigned tot = 0;
-
-	if(pc){
-		pkglist *pl;
-
-		for(pl = pc->lists ; pl ; pl = pl ->next){
-			tot += pl->pcount;
-		}
-	}
-	return tot;
 }
