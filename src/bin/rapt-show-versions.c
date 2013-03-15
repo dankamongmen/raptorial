e9c97b0639874c076eb8c7499fee7a92ad95b6f8
nick black <nick.black@sprezzatech.com>
2013-03-15 05:49:13 -0400

    split out filtered_output

--- src/bin/rapt-show-versions.c
+++ src/bin/rapt-show-versions.c
@@ -14,27 +14,50 @@ usage(FILE *out,const char *name){
 	fprintf(out,"options:\n");
 	fprintf(out," -s|--status-file=<file> Status file (def: %s)\n",raptorial_def_status_file());
 	fprintf(out," -l|--list-dir=<dir>     List directory (def: %s)\n",raptorial_def_lists_dir());
-	fprintf(out," -v|--verbose            Increase verbosity during parsing\n");
 	fprintf(out," -a|--allversions        Print all available versions\n");
 	fprintf(out," -h|--help               Display this usage summary\n");
 }
 
+#include <assert.h>
+static int
+filtered_output(const char **argv,struct pkgcache *pc,struct pkglist *stat,
+					int allversions){
+	const struct pkgobj *po;
+
+	assert(pc);
+	do{
+		if((po = pkglist_find(stat,*argv)) == NULL){
+			if(!allversions){
+				if(printf("%s not installed\n",*argv) < 0){
+					return -1;
+				}
+			}else{
+				// FIXME print all versions
+			}
+		}else{
+			// FIXME print status
+		}
+	}while(*++argv);
+	return 0;
+}
+
+// There's no need to free up the structures on exit -- the OS reclaims that
+// memory. If this code is embedded elsewhere, however, make use of
+// free_package_list() and free_package_cache() as appropriate.
 int main(int argc,char **argv){
 	const struct option longopts[] = {
 		{ "status-file", 1, NULL, 's' },
 		{ "list-dir", 1, NULL, 'l' },
-		{ "verbose", 0, NULL, 'v' },
 		{ "allversions", 0, NULL, 'a' },
 		{ "help", 0, NULL, 'h' },
 		{ NULL, 0, NULL, 0 }
 	};
 	const char *listdir,*statusfile;
+	struct pkglist *stat;
+	int allversions = 0;
 	struct pkgcache *pc;
-	struct pkglist *pl;
-	struct pkgobj *po;
 	int err,c;
 
-	//verbose = 0;
 	listdir = NULL;
 	statusfile = NULL;
 	while((c = getopt_long(argc,argv,"s:l:p:vah",longopts,&optind)) != -1){
@@ -43,10 +66,7 @@ int main(int argc,char **argv){
 				usage(stdout,argv[0]);
 				return EXIT_SUCCESS;
 			case 'a':
-				fprintf(stderr,"Not yet implemented: --allversions\n");
-				return EXIT_FAILURE; // FIXME
-			case 'v':
-				// verbose = 1;
+				allversions = 1;
 				break;
 			case 'l':
 				if(listdir){
@@ -74,7 +94,7 @@ int main(int argc,char **argv){
 	if(listdir == NULL){
 		listdir = raptorial_def_lists_dir();
 	}
-	if(parse_status_file(statusfile,&err)){
+	if((stat = parse_status_file(statusfile,&err)) == NULL){
 		fprintf(stderr,"Couldn't parse %s (%s?)\n",statusfile,strerror(err));
 		return EXIT_FAILURE;
 	}
@@ -82,26 +102,23 @@ int main(int argc,char **argv){
 		fprintf(stderr,"Couldn't parse %s (%s?)\n",listdir,strerror(err));
 		return EXIT_FAILURE;
 	}
-	for(pl = pkgcache_begin(pc) ; pl ; pl = pkgcache_next(pl)){
-		for(po = pkglist_begin(pl) ; po ; po = pkglist_next(po)){
-			char **filters = argv + optind;
+	if(argv[optind]){
+		if(filtered_output((const char **)(argv + optind),pc,stat,allversions) < 0){
+			return EXIT_FAILURE;
+		}
+	}else{
+		const struct pkglist *pl;
 
-			if(*filters){
-				do{
-					if(strcmp(*filters,pkgobj_name(po)) == 0){
-						break;
-					}
-				}while(*++filters);
-				if(!*filters){
-					continue;
-				}
+		for(pl = pkgcache_begin(pc) ; pl ; pl = pkgcache_next(pl)){
+			const struct pkgobj *po;
+
+			for(po = pkglist_begin(pl) ; po ; po = pkglist_next(po)){
+				// Associate with package from status file and print
+				// upgrade info as available FIXME
+				printf("%s/%s %s\n",pkgobj_name(po),
+					pkglist_dist(pl),pkgobj_version(po));
 			}
-			// Associate with package from status file and print
-			// upgrade info as available FIXME
-			printf("%s/%s %s\n",pkgobj_name(po),
-				pkglist_dist(pl),pkgobj_version(po));
 		}
 	}
-	free_package_cache(pc);
 	return EXIT_SUCCESS;
 }
