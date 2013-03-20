e474a9ad0fbbad4bc1430c7de1a8674fe5c5359a
nick black <nick.black@sprezzatech.com>
2013-03-19 21:24:29 -0400

    add some tests to unittester

--- src/tester/main.c
+++ src/tester/main.c
@@ -24,13 +24,14 @@ int main(int argc,char **argv){
 		fprintf(stderr,"Couldn't parse %s (%s?)\n",argv[1],strerror(err));
 		return EXIT_FAILURE;
 	}
-	pkgs = 0;
 	dfa = NULL;
+	pkgs = 0;
 	printf("Reported package count: %u\n",pkgcache_count(pc));
 	for(pl = pkgcache_begin(pc) ; pl ; pl = pkgcache_next(pl)){
 		for(po = pkglist_begin(pl) ; po ; po = pkglist_next(po)){
 			printf("%s %s\n",pkgobj_name(po),pkgobj_version(po));
 			if(augment_dfa(&dfa,pkgobj_name(po),main)){
+				fprintf(stderr,"Error augmenting DFA (%s)\n",pkgobj_name(po));
 				return EXIT_FAILURE;
 			}
 			++pkgs;
@@ -43,6 +44,28 @@ int main(int argc,char **argv){
 	}
 	free_package_cache(pc);
 	free_dfa(dfa);
+
+	dfa = NULL;
+	if((pc = pkgcache_from_pkglist(lex_packages_file(argv[1],&err,&dfa),&err)) == NULL){
+		fprintf(stderr,"Couldn't parse %s (%s?)\n",argv[1],strerror(err));
+		return EXIT_FAILURE;
+	}
+	pkgs = 0;
+	printf("Reported package count: %u\n",pkgcache_count(pc));
+	for(pl = pkgcache_begin(pc) ; pl ; pl = pkgcache_next(pl)){
+		for(po = pkglist_begin(pl) ; po ; po = pkglist_next(po)){
+			printf("%s %s\n",pkgobj_name(po),pkgobj_version(po));
+			++pkgs;
+		}
+	}
+	if(pkgs != pkgcache_count(pc)){
+		fprintf(stderr,"Package count was inaccurate (%u != %u)\n",
+				pkgs,pkgcache_count(pc));
+		return EXIT_FAILURE;
+	}
+	free_package_cache(pc);
+	free_dfa(dfa);
+
 	printf("Successfully parsed %s (%u package%s)\n",argv[1],
 			pkgs,pkgs == 1 ? "" : "s");
 	return EXIT_SUCCESS;
