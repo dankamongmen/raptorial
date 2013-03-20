d99c9545b6fe48785201a5594c255e434953e69e
nick black <nick.black@sprezzatech.com>
2013-03-20 15:02:06 -0400

    add FIXME regarding deb-version comparisons

--- src/lib/versioncmp.c
+++ src/lib/versioncmp.c
@@ -17,6 +17,24 @@ debccmp(int d1,int d2){
 
 
 // See deb-version(5) for the full details of comparing Debian version strings
+// FIXME doesn't handle numeric comparison part correctly:
+/*
+       First the initial part of each string consisting entirely of  non-digit
+       characters  is determined.  These two parts (one of which may be empty)
+       are compared lexically.  If a difference is found it is returned.   The
+       lexical comparison is a comparison of ASCII values modified so that all
+       the letters sort earlier than all the non-letters and so that  a  tilde
+       sorts  before  anything, even the end of a part.  For example, the fol‐
+       lowing parts are in sorted order: '~~', '~~a',  '~',  the  empty  part,
+       'a'.
+
+       Then  the  initial  part of the remainder of each string which consists
+       entirely of digit characters is determined.  The  numerical  values  of
+       these  two  parts are compared, and any difference found is returned as
+       the result of the comparison.   For  these  purposes  an  empty  string
+       (which  can  only occur at the end of one or both version strings being
+       compared) counts as zero.*/
+
 int debcmp(const char *d1,const char *d2){
 	while(*d1 || *d2){
 		int r = debccmp(*d1,*d2);
