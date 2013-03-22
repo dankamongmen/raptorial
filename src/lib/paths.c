e3e130d0fec30d082c31f962af14084842e29f1a
nick black <nick.black@sprezzatech.com>
2013-03-21 21:26:45 -0400

    add raptorial_def_content_dir()

--- src/lib/paths.c
+++ src/lib/paths.c
@@ -1,7 +1,8 @@
 #include <raptorial.h>
 
-#define STATUSFILE_DEFAULT "/var/lib/dpkg/status"
 #define LISTDIR_DEFAULT "/var/lib/apt/lists"
+#define STATUSFILE_DEFAULT "/var/lib/dpkg/status"
+#define CONTENTDIR_DEFAULT "/var/cache/apt/apt-file"
 
 const char *raptorial_def_lists_dir(void){
 	return LISTDIR_DEFAULT;
@@ -9,4 +10,8 @@ const char *raptorial_def_lists_dir(void){
 
 const char *raptorial_def_status_file(void){
 	return STATUSFILE_DEFAULT;
+}
+
+const char *raptorial_def_content_dir(void){
+	return CONTENTDIR_DEFAULT;
 }
