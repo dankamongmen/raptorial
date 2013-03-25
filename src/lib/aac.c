0dff7623a4d9a156c209772ef61eeb774e4718eb
nick black <nick.black@sprezzatech.com>
2013-03-24 23:09:41 -0400

    only run make_delta2() once

--- src/lib/aac.c
+++ src/lib/aac.c
@@ -88,6 +88,46 @@ edge_search(const dfavtx *node,int s){
 	return pos;
 }
 
+static inline unsigned
+is_prefix(const unsigned char *word,int wordlen,int pos){
+	unsigned suffixlen = wordlen - pos,z;
+
+	for(z = 0 ; z < suffixlen; ++z){
+		if(word[z] != word[pos + z]){
+			return 0;
+		}
+	}
+	return 1;
+}
+ 
+static inline unsigned
+suffix_length(const unsigned char *s,unsigned len,unsigned pos){
+	unsigned z;
+
+	for(z = 0 ; (s[pos - z] == s[len - 1 - z]) && (z < pos); ++z);
+	return z;
+}
+ 
+static inline void
+make_delta2(int *delta2, const unsigned char *pat, int32_t patlen) {
+	int p;
+	int last_prefix_index = patlen-1;
+
+	for (p=patlen-1; p>=0; p--) {
+		if (is_prefix(pat, patlen, p+1)) {
+			last_prefix_index = p+1;
+		}
+		delta2[p] = last_prefix_index + (patlen-1 - p);
+	}
+	for (p=0; p < patlen-1; p++) {
+		int slen = suffix_length(pat, patlen, p);
+
+		if (pat[p - slen] != pat[patlen-1 - slen]) {
+			delta2[patlen-1 - slen] = patlen-1 - p + slen;
+		}
+	}
+}
+
 static int
 bmgprepare(bmgstate *bmg,const char *str,void *val){
 	unsigned z;
@@ -107,6 +147,7 @@ bmgprepare(bmgstate *bmg,const char *str,void *val){
 	for(z = 0 ; z < bmg->len - 1 ; ++z){
 		bmg->delta1[bmg->match[z]] = bmg->len - 1 - z;
 	}
+	make_delta2(bmg->delta2,bmg->match,bmg->len);
 	return 0;
 }
 
@@ -234,92 +275,8 @@ void *match_dfactx_nstring(dfactx *dctx,const char *s,size_t len){
 	return dctx->dfa->vtxarray[dctx->cur].val;
 }
 
-#define NOT_FOUND patlen
-#define max(a, b) ((a < b) ? b : a)
- 
-// true if the suffix of word starting from word[pos] is a prefix 
-// of word
-int is_prefix(const uint8_t *word, int wordlen, int pos) {
-	int i;
-	int suffixlen = wordlen - pos;
-	// could also use the strncmp() library function here
-	for (i = 0; i < suffixlen; i++) {
-		if (word[i] != word[pos+i]) {
-			return 0;
-		}
-	}
-	return 1;
-}
- 
-// length of the longest suffix of word ending on word[pos].
-// suffix_length("dddbcabc", 8, 4) = 2
-int suffix_length(const uint8_t *word, int wordlen, int pos) {
-	int i;
-	// increment suffix length i to the first mismatch or beginning
-	// of the word
-	for (i = 0; (word[pos-i] == word[wordlen-1-i]) && (i < pos); i++);
-	return i;
-}
- 
-// delta2 table: given a mismatch at pat[pos], we want to align 
-// with the next possible full match could be based on what we
-// know about pat[pos+1] to pat[patlen-1].
-//
-// In case 1:
-// pat[pos+1] to pat[patlen-1] does not occur elsewhere in pat,
-// the next plausible match starts at or after the mismatch.
-// If, within the substring pat[pos+1 .. patlen-1], lies a prefix
-// of pat, the next plausible match is here (if there are multiple
-// prefixes in the substring, pick the longest). Otherwise, the
-// next plausible match starts past the character aligned with 
-// pat[patlen-1].
-// 
-// In case 2:
-// pat[pos+1] to pat[patlen-1] does occur elsewhere in pat. The
-// mismatch tells us that we are not looking at the end of a match.
-// We may, however, be looking at the middle of a match.
-// 
-// The first loop, which takes care of case 1, is analogous to
-// the KMP table, adapted for a 'backwards' scan order with the
-// additional restriction that the substrings it considers as 
-// potential prefixes are all suffixes. In the worst case scenario
-// pat consists of the same letter repeated, so every suffix is
-// a prefix. This loop alone is not sufficient, however:
-// Suppose that pat is "ABYXCDEYX", and text is ".....ABYXCDEYX".
-// We will match X, Y, and find B != E. There is no prefix of pat
-// in the suffix "YX", so the first loop tells us to skip forward
-// by 9 characters.
-// Although superficially similar to the KMP table, the KMP table
-// relies on information about the beginning of the partial match
-// that the BM algorithm does not have.
-//
-// The second loop addresses case 2. Since suffix_length may not be
-// unique, we want to take the minimum value, which will tell us
-// how far away the closest potential match is.
-void make_delta2(int *delta2, const unsigned char *pat, int32_t patlen) {
-    int p;
-    int last_prefix_index = patlen-1;
- 
-    // first loop
-    for (p=patlen-1; p>=0; p--) {
-        if (is_prefix(pat, patlen, p+1)) {
-            last_prefix_index = p+1;
-        }
-        delta2[p] = last_prefix_index + (patlen-1 - p);
-    }
- 
-    // second loop
-    for (p=0; p < patlen-1; p++) {
-        int slen = suffix_length(pat, patlen, p);
-        if (pat[p - slen] != pat[patlen-1 - slen]) {
-            delta2[patlen-1 - slen] = patlen-1 - p + slen;
-        }
-    }
-}
-
 int bmg_against_nstring(const unsigned char *s,size_t len,const bmgstate *bmg){
 	size_t i;
-	make_delta2(bmg->delta2, bmg->match, bmg->len);
 
 	i = bmg->len - 1;
 	while (i < len) {
@@ -332,7 +289,8 @@ int bmg_against_nstring(const unsigned char *s,size_t len,const bmgstate *bmg){
 			return 1;
 		}
 
-		i += max(bmg->delta1[s[i]],bmg->delta2[j]);
+		i += bmg->delta1[s[i]] >= bmg->delta2[j] ? 
+			bmg->delta1[s[i]] : bmg->delta2[j];
 	}
 	return 0;
 }
